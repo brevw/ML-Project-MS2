6367d2f1b6c89823597e7b40ed0b54fae1f10090
akrout123 <cyrine.akrout@epfl.ch>
2024-05-19 13:44:02 +0200

    mlp and cnn plotting

--- 344929_344939_344848_project/main.py
+++ 344929_344939_344848_project/main.py
@@ -2,9 +2,12 @@ import argparse
 
 import numpy as np
 from torchinfo import summary
+import time
+from matplotlib import pyplot as plt
 
 from src.data import load_data
 from src.methods.pca import PCA
+from src.methods.dummy_methods import DummyClassifier
 from src.methods.deep_network import MLP, CNN, Trainer, MyViT
 from src.utils import normalize_fn, append_bias_term, accuracy_fn, macrof1_fn, get_n_classes
 
@@ -23,6 +26,9 @@ def main(args):
     xtest = xtest.reshape(xtest.shape[0], -1)
     ## 2. Then we must prepare it. This is were you can create a validation set,
     #  normalize, add bias, etc.
+
+    if args.plotMLP:
+        args.test = False
 
     # Make a validation set
     if not args.test:
@@ -54,6 +60,110 @@ def main(args):
 
         ### WRITE YOUR CODE HERE: use the PCA object to reduce the dimensionality of the data
 
+    # plotting functions
+    if args.plotMLP:
+        n_classes = get_n_classes(ytrain)
+
+        # without pca
+        model = MLP(xtrain.shape[1], n_classes)
+        average_loss_epoch_list_without_pca = []
+        method_obj = Trainer(model, lr=args.lr, epochs=args.max_iters, batch_size=args.nn_batch_size, average_loss_list=average_loss_epoch_list_without_pca)
+        train_without_pca_start = time.time()
+        preds_train = method_obj.fit(xtrain, ytrain)
+        train_without_pca_stop = time.time()
+        preds = method_obj.predict(xtest)
+        acc = accuracy_fn(preds_train, ytrain)
+        macrof1 = macrof1_fn(preds_train, ytrain)
+        print(f"\nTrain set without: accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
+        acc_without_pca = accuracy_fn(preds, ytest)
+        macrof1_without_pca = macrof1_fn(preds, ytest)
+        print(f"Validation set without pca:  accuracy = {acc_without_pca:.3f}% - F1-score = {macrof1_without_pca:.6f}")
+
+        # with pca
+        print("Using PCA")
+        pca_obj = PCA(d=args.pca_d)
+        print(f'The total variance explained by the first {args.pca_d} principal components is {pca_obj.find_principal_components(xtrain):.3f} %')
+        xtrain = pca_obj.reduce_dimension(xtrain)
+        xtest = pca_obj.reduce_dimension(xtest)
+
+        model = MLP(xtrain.shape[1], n_classes)
+        average_loss_epoch_list_with_pca = []
+        method_obj = Trainer(model, lr=args.lr, epochs=args.max_iters, batch_size=args.nn_batch_size, average_loss_list=average_loss_epoch_list_with_pca)
+        train_with_pca_start = time.time()
+        preds_train = method_obj.fit(xtrain, ytrain)
+        train_with_pca_stop = time.time()
+        preds = method_obj.predict(xtest)
+        acc = accuracy_fn(preds_train, ytrain)
+        macrof1 = macrof1_fn(preds_train, ytrain)
+        print(f"\nTrain set with: accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
+        acc_with_pca = accuracy_fn(preds, ytest)
+        macrof1_with_pca = macrof1_fn(preds, ytest)
+        print(f"Validation set with pca:  accuracy = {acc_with_pca:.3f}% - F1-score = {macrof1_with_pca:.6f}")
+
+        # plotting
+        nbr_epoc = np.arange(1, args.max_iters + 1)
+        plt.figure()
+        plt.title("Performance analysis considering PCA")
+        skip_factor = 5
+        plt.plot(nbr_epoc[::skip_factor], average_loss_epoch_list_without_pca[::skip_factor], 'ro-' , label = f"w/o PCA: Time running {train_without_pca_stop - train_without_pca_start:.2f} - acc: {acc_without_pca:.2f} - f1: {macrof1_without_pca:.2f}")
+        plt.plot(nbr_epoc[::skip_factor], average_loss_epoch_list_with_pca[::skip_factor], 'bo-', label = f"w/ PCA: Time running {train_with_pca_stop - train_with_pca_start:.2f} - acc: {acc_with_pca:.2f} - f1: {macrof1_with_pca:.2f}")
+        plt.ylabel("average loss")
+        plt.xlabel("epoch")
+        plt.legend()
+        plt.show()
+        exit(0)
+
+    if args.plotCNN:
+        n_classes = get_n_classes(ytrain)
+
+        # lr = 1e-5, batch_size = 64
+        args.lr = 1e-5
+        
+        model = CNN(1, n_classes)
+        average_loss_epoch_list_1 = []
+        method_obj = Trainer(model, lr=args.lr, epochs=args.max_iters, batch_size=args.nn_batch_size, average_loss_list=average_loss_epoch_list_1)
+        train_start_1 = time.time()
+        preds_train = method_obj.fit(xtrain, ytrain)
+        train_stop_1 = time.time()
+        preds = method_obj.predict(xtest)
+        acc = accuracy_fn(preds_train, ytrain)
+        macrof1 = macrof1_fn(preds_train, ytrain)
+        print(f"\nTrain set without: accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
+        acc_1 = accuracy_fn(preds, ytest)
+        macrof1_1 = macrof1_fn(preds, ytest)
+        print(f"Validation set without pca:  accuracy = {acc_1:.3f}% - F1-score = {macrof1_1:.6f}")
+
+        # lr = 1e-3 
+        args.lr = 1e-3
+
+        model = CNN(1, n_classes)
+        average_loss_epoch_list_2 = []
+        method_obj = Trainer(model, lr=args.lr, epochs=args.max_iters, batch_size=args.nn_batch_size, average_loss_list=average_loss_epoch_list_2)
+        train_start_2 = time.time()
+        preds_train = method_obj.fit(xtrain, ytrain)
+        train_stop_2 = time.time()
+        preds = method_obj.predict(xtest)
+        acc = accuracy_fn(preds_train, ytrain)
+        macrof1 = macrof1_fn(preds_train, ytrain)
+        print(f"\nTrain set with: accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
+        acc_2 = accuracy_fn(preds, ytest)
+        macrof1_2 = macrof1_fn(preds, ytest)
+        print(f"Validation set with pca:  accuracy = {acc_2:.3f}% - F1-score = {macrof1_2:.6f}")
+
+        # plotting
+        nbr_epoc = np.arange(1, args.max_iters + 1)
+        plt.figure()
+        plt.title("Performance analysis considering PCA")
+        skip_factor = 5
+        plt.plot(nbr_epoc[::skip_factor], average_loss_epoch_list_1[::skip_factor], 'ro-' , label = f"w/ lr -> 1e-5: Time running {train_start_1 - train_stop_1:.2f} - acc: {acc_1:.2f} - f1: {macrof1_1:.2f}")
+        plt.plot(nbr_epoc[::skip_factor], average_loss_epoch_list_2[::skip_factor], 'bo-', label = f"w/ lr -> 1e-3: Time running {train_start_2 - train_stop_2:.2f} - acc: {acc_2:.2f} - f1: {macrof1_2:.2f}")
+        plt.ylabel("average loss")
+        plt.xlabel("epoch")
+        plt.legend()
+        plt.show()
+        exit(0)
+
+
 
     ## 3. Initialize the method you want to use.
 
@@ -69,11 +179,12 @@ def main(args):
     elif args.nn_type == "transformer":
         model = MyViT((1, 28, 28), 7, 2, 8, 2, n_classes)
     else :
-        pass
+        model = DummyClassifier(0) 
     summary(model)
 
     # Trainer object
-    method_obj = Trainer(model, lr=args.lr, epochs=args.max_iters, batch_size=args.nn_batch_size)
+    average_loss_epoch_list = []
+    method_obj = Trainer(model, lr=args.lr, epochs=args.max_iters, batch_size=args.nn_batch_size, average_loss_list=average_loss_epoch_list)
 
 
     ## 4. Train and evaluate the method
@@ -109,6 +220,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # Feel free to add more arguments here if you need!
     parser.add_argument('--method', default="dummy-classifier", type=str, help="method to run")
+    parser.add_argument('--plotMLP', action="store_true",
+                        help="show performance graph (kinda like cross-validation) on a MLP model")
+    parser.add_argument('--plotCNN', action="store_true",
+                        help="show performance graph (kinda like cross-validation) on a MLP model")
+    
 
     # MS2 arguments
     parser.add_argument('--data', default="dataset", type=str, help="path to your dataset")
